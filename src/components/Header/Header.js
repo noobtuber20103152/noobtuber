f3367ba624b7be5d0e46eae13fea24ffd8e3ef88
ujjawal <ujjawalrachhoya100@gmail.com>
2022-06-12 20:07:34 +0530

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,8 @@ import React, { useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function Header() {
-    const [copy, setcopy] = useState("true");
+    const [copy, setcopy] = useState("");
     const onclick = () => {
-
         setcopy(false);
         let text = window.location.href;
         navigator.clipboard.writeText(text)
@@ -31,7 +30,7 @@ function Header() {
                                     <p className='leading-relaxed text-ellipsis font-bold'>MERN stack developer</p>
                                     <p class=" hover:cursor-pointer leading-relaxed text-base">CSE Sophomore, Quick learner, Self motivated, Enthusiastic</p>
                                     <div className='flex flex-row'>
-                                        <a target="_blank" href="https://drive.google.com/file/d/1lSKPqVVwjYJWXC7g5mlIVzLVfakYtaj9/view?usp=sharing" style={{ alignItems: 'center' }} className='hover:text-blue-500 h-11 w-11 hover:bg-blue-100  flex justify-center  rounded-full '>
+                                        <a target="_blank" href="https://drive.google.com/file/d/1bjn6RVfMfdEhfWM5Kz3PG-oFzqF-FYlq/view?usp=sharing" style={{ alignItems: 'center' }} className='hover:text-blue-500 h-11 w-11 hover:bg-blue-100  flex justify-center  rounded-full '>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
                                                 <path fill='blue' fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
                                             </svg>
