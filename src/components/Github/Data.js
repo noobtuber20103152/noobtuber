00a5cf53b57a9d5c07837e9e39c3ae7135ce9797
ujjawal <ujjawalrachhoya100@gmail.com>
2022-06-20 00:49:36 +0530

    date 19 june

--- src/components/Github/Data.js
+++ src/components/Github/Data.js
@@ -1,6 +1,6 @@
 let datapoints = [];
 let may = [9, 0, 11, 3]
 datapoints = datapoints.concat(may)
-let june = [1, 1, 14, 2, 1, 4, 3, 3, 6, 6, 14, 4, 11,7,1,1]
+let june = [1, 1, 14, 2, 1, 4, 3, 3, 6, 6, 14, 4, 11,7,1,1,3,4,2]
 datapoints = datapoints.concat(june)
 export default datapoints;
